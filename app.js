906533629d5a2ecc57bcaa4b3a89d1262a7ed09f
Cameron Wright <cam.mac.wright@gmail.com>
2020-12-06 18:17:14 -0800

    starting js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 $(document).ready(function () {
-    
-}
+    const currentTime = moment().format('MMMM Do YYYY, h:mm a');
+    $("#day-today").text(currentTime)
+    const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+})
